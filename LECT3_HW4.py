d2fbc36360fa28390799d126cf5b9fdddcd74b79
Yulia Magonya <ymagonia@gmail.com>
2024-04-19 03:11:29 +0300

    Update LECT3_HW4.py
    working..

--- LECT3_HW4.py
+++ LECT3_HW4.py
@@ -1,11 +1,1 @@
-#
 
-# mi_i = int(input("Enter minimal width: "))
-# ma_i = int(input("Enter maximal width: "))
-#
-# for i in range(ma_i):
-#     if mi_i > ma_i:
-#         print("Error!")
-#         break
-# else:
-#     print("test")

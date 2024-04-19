2e46064324f687d4e3fb209e2aee376ca5a70fb7
Yulia Magonya <ymagonia@gmail.com>
2024-04-19 03:09:06 +0300

    aaaaaaaaa...

---
+++ LECT3_HW5.py
@@ -0,0 +1,13 @@
+
+n = int(input("Enter n: "))
+
+for i in range(1, n + 2):
+    for j in range(n - i + 1):
+        print(end="  ")
+    for j in range(1, i - 1):
+        print(j, end=" ")
+    for j in range(i - 1, 0, -1):
+        print(j, end=" ")
+
+    print("")
+

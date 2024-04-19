5eed835553b7460b5aaa30fbbe6ae2d931ee3f66
Yulia Magonya <ymagonia@gmail.com>
2024-04-19 22:50:45 +0300

    hw3_correct

--- LECT3_HW3.py
+++ LECT3_HW3.py
@@ -1,10 +1,21 @@
 
+# a = "*"
+# b = " "
+# i = int(input("Enter the size of the triangle: "))
+# j = 1
+#
+# for k in range(i, 0, -1):
+#     print((b * k) + (a * j))
+#     j += 1
+
 a = "*"
 b = " "
 i = int(input("Enter the size of the triangle: "))
 j = 1
 
 for k in range(i, 0, -1):
+    k -= 1
     print((b * k) + (a * j))
     j += 1
+
 

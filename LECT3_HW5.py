ffa13eae1f456d59aca3f277db2abcfa3dc0f5f8
Yulia Magonya <ymagonia@gmail.com>
2024-04-22 20:39:19 +0300

    l3_hw5_redone

--- LECT3_HW5.py
+++ LECT3_HW5.py
@@ -7,10 +7,10 @@ for i in range(2, n + 2):
     for j in range(1, i - 1):
         print(j, end=" ")
     for j in range(i - 1, 0, -1):
-        print(j, end=" ")
+        print(j, end="")
+        for k in range(j - 1, 0, -j):
+            print(end=" ")
 
     print()
-
-
 
 
